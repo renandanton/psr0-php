fbfaec9bab9f7aca1ba5ae153f1412cd70bcf70c
Renan Danton <renandanton@yahoo.com.br>
2016-02-22 14:36:25 +0000

    Organizando pastas

--- public/config.php
+++ public/config.php
@@ -2,6 +2,11 @@
 
 $container = new Pimple\Container();
 
+//$container['host'] = 'localhost';
+//$container['db'] = 'u778176460_storm';
+//$container['user'] = 'u778176460_storm';
+//$container['password'] = 'danton123';
+
 $container['host'] = 'localhost';
 $container['db'] = 'test';
 $container['user'] = 'homestead';
